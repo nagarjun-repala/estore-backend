6f7ff189ed2ee6ca89e7c6d3426d1e5fb5e72cf5
nagarjun-repala <nagarjun.repala@outlook.com>
2022-11-06 20:53:19 +0530

    Initializing the project

---
+++ src/main/java/com/nagarjun/estorebackend/EstoreBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nagarjun.estorebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstoreBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstoreBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nagarjun/estorebackend/EstoreBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nagarjun.estorebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstoreBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
