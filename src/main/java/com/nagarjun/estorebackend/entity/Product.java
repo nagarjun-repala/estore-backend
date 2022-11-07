72f909b63da20c520a52dd770bbb8200112d4a86
nagarjun-repala <nagarjun.repala@outlook.com>
2022-11-07 19:54:43 +0530

    Updated Product entity

--- src/main/java/com/nagarjun/estorebackend/entity/Product.java
+++ src/main/java/com/nagarjun/estorebackend/entity/Product.java
@@ -1,5 +1,35 @@
 package com.nagarjun.estorebackend.entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+@AllArgsConstructor
+@NoArgsConstructor
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NonNull
+    private String name;
+
+    @NonNull
+    private String description;
+
+    @NonNull
+    private Float price;
+
+    private List<Review> reviews;
+
+
     
 }
