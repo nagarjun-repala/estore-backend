41c40eb02dcb76489b7340e1301890c84d331ef3
nagarjun-repala <nagarjun.repala@outlook.com>
2022-11-07 19:56:36 +0530

    Updated User entity

--- src/main/java/com/nagarjun/estorebackend/entity/User.java
+++ src/main/java/com/nagarjun/estorebackend/entity/User.java
@@ -1,5 +1,45 @@
 package com.nagarjun.estorebackend.entity;
+import java.sql.Date;
+import java.util.List;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+import lombok.*;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+@NoArgsConstructor
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NonNull
+    private String firstName;
+
+    @NonNull
+    private String lastName;
+
+    @NonNull
+    private String username;
+
+    @NonNull
+    private String password;
+
+    @NonNull
+    private String email;
+
+    @NonNull
+    private Date createdOn;
+
+    private List<Product> orders;
+
+
+    private List<Product> cart;
     
 }
