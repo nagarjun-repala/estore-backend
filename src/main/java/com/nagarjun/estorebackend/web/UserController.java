f47cf3f9075bc88f2502c92b7f3ad04010078486
nagarjun-repala <nagarjun.repala@outlook.com>
2022-11-08 17:33:06 +0530

    Updated user controller with methods

--- src/main/java/com/nagarjun/estorebackend/web/UserController.java
+++ src/main/java/com/nagarjun/estorebackend/web/UserController.java
@@ -1,5 +1,47 @@
 package com.nagarjun.estorebackend.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.nagarjun.estorebackend.entity.User;
+import com.nagarjun.estorebackend.service.UserService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@RestController
+@RequestMapping("/user")
 public class UserController {
+
+    @Autowired
+    UserService userService;
+    
+    @GetMapping("/{id}")
+    public ResponseEntity<HttpStatus> findById(@PathVariable Long id) {
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+    
+    @PostMapping("/register")
+    public ResponseEntity<HttpStatus> createUser(@RequestBody User user) {
+
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<HttpStatus> updateUser(@RequestBody User user) {
+        return new ResponseEntity<>(HttpStatus.OK);
+
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<HttpStatus> getUsers() {
+        return new ResponseEntity<>(HttpStatus.OK);
+
+    }
     
 }
