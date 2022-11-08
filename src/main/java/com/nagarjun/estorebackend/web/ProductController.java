bf292d16d568266a7c77759b4af9bc755f0fd661
nagarjun-repala <nagarjun.repala@outlook.com>
2022-11-08 17:33:46 +0530

    Updated product controller with get method

--- src/main/java/com/nagarjun/estorebackend/web/ProductController.java
+++ src/main/java/com/nagarjun/estorebackend/web/ProductController.java
@@ -1,5 +1,18 @@
 package com.nagarjun.estorebackend.web;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@RestController
+@RequestMapping("/product")
 public class ProductController {
-    
+
+    @GetMapping("/{id}")
+    public ResponseEntity<String> findById(@PathVariable Long id) {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
