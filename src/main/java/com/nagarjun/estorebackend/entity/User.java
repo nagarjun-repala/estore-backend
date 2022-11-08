fabdcaa189742911ccc892ff6494bd246bce6b1d
nagarjun-repala <nagarjun.repala@outlook.com>
2022-11-08 17:19:22 +0530

    Removed order and cart properties from User entity

--- src/main/java/com/nagarjun/estorebackend/entity/User.java
+++ src/main/java/com/nagarjun/estorebackend/entity/User.java
@@ -1,18 +1,20 @@
 package com.nagarjun.estorebackend.entity;
-import java.sql.Date;
-import java.util.List;
 
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
-
+import javax.persistence.Table;
 import lombok.*;
 
 @Getter
 @Setter
 @RequiredArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "users")
 public class User {
 
     @Id
@@ -20,26 +22,27 @@ public class User {
     private long id;
 
     @NonNull
+    @Column(name = "first_name", nullable = false)
     private String firstName;
 
     @NonNull
+    @Column(name = "last_name", nullable = false)    
     private String lastName;
 
     @NonNull
+    @Column(nullable = false)    
     private String username;
 
     @NonNull
+    @Column(nullable = false)    
     private String password;
 
     @NonNull
+    @Column(nullable = false)
     private String email;
 
     @NonNull
+    @Column(name = "created_on", nullable = false)
     private Date createdOn;
-
-    private List<Product> orders;
-
-
-    private List<Product> cart;
     
 }
