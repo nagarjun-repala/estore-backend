5217394a335a520f6ccac18bd27f0d5355d1c9cd
nagarjun-repala <nagarjun.repala@outlook.com>
2022-11-08 17:26:59 +0530

    Created Order entity

---
+++ src/main/java/com/nagarjun/estorebackend/entity/Order.java
@@ -0,0 +1,40 @@
+package com.nagarjun.estorebackend.entity;
+
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.*;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "order")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NonNull
+    @Column(name = "created_on", nullable = false)
+    private Date createdOn;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "product_id", referencedColumnName = "id")
+    private Product product;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+
+    
+}
