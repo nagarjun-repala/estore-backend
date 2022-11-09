16220d7a80886863806fb1678323d074bd0ded30
nagarjun-repala <nagarjun.repala@outlook.com>
2022-11-10 02:32:34 +0530

    Removed SQL keyword from table name

--- src/main/java/com/nagarjun/estorebackend/entity/Order.java
+++ src/main/java/com/nagarjun/estorebackend/entity/Order.java
@@ -1,13 +1,12 @@
 package com.nagarjun.estorebackend.entity;
 
 import java.util.Date;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import lombok.*;
@@ -15,26 +14,19 @@ import lombok.*;
 @Getter
 @Setter
 @RequiredArgsConstructor
+@AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table(name = "order")
+@Table(name = "orders")
 public class Order {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    @Column(name = "id")
+    private Long id;
 
     @NonNull
     @Column(name = "created_on", nullable = false)
     private Date createdOn;
-
-    @ManyToOne(optional = false)
-    @JoinColumn(name = "product_id", referencedColumnName = "id")
-    private Product product;
-
-    @ManyToOne(optional = false)
-    @JoinColumn(name = "user_id", referencedColumnName = "id")
-    private User user;
-
     
 }
