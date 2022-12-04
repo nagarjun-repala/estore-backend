191ceae0933c1cec419cf63a24ffacd089533158
nagarjun-repala <nagarjun.repala@outlook.com>
2022-12-04 21:34:18 +0530

    Add basic auth for the application

--- src/main/java/com/nagarjun/estorebackend/security/SecurityConfig.java
+++ src/main/java/com/nagarjun/estorebackend/security/SecurityConfig.java
@@ -1,29 +1,57 @@
 package com.nagarjun.estorebackend.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
             .csrf().disable()
             .authorizeRequests()
+            .antMatchers(HttpMethod.DELETE).hasRole("ADMIN")
+            .antMatchers(HttpMethod.POST).hasAnyRole("ADMIN", "USER")
             .antMatchers(HttpMethod.GET).permitAll()
-            .antMatchers(HttpMethod.POST).permitAll()
-            .antMatchers(HttpMethod.PUT).permitAll()
-            .antMatchers(HttpMethod.DELETE).permitAll()
-            .antMatchers(HttpMethod.PATCH).permitAll()
             .anyRequest().authenticated()
             .and()
-            .httpBasic();
-
+            .httpBasic()
+            .and()
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService users(){
+        UserDetails admin = User.builder()
+            .username("admin")
+            .password(bCryptPasswordEncoder.encode("admin"))
+            .roles("ADMIN")
+            .build();
+
+        UserDetails user = User.builder()
+        .username("user")
+        .password(bCryptPasswordEncoder.encode("user"))
+        .roles("USER")
+        .build();
+
+        return new InMemoryUserDetailsManager(admin, user);
+
     }
     
 }
