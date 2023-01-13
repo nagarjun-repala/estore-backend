009c7dad96e17f1f7138fb9c174e49af73c11ee5
Nagarjun <nagarjun.repala@outlook.com>
2023-01-13 11:44:21 +0530

    Add test user for testing

--- src/main/java/com/nagarjun/estorebackend/EstoreBackendApplication.java
+++ src/main/java/com/nagarjun/estorebackend/EstoreBackendApplication.java
@@ -8,9 +8,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.boot.CommandLineRunner;
+import com.nagarjun.estorebackend.entity.Cart;
 import com.nagarjun.estorebackend.entity.Product;
 import com.nagarjun.estorebackend.entity.Role;
 import com.nagarjun.estorebackend.entity.User;
+import com.nagarjun.estorebackend.repository.CartRepository;
 import com.nagarjun.estorebackend.repository.ProductRepository;
 import com.nagarjun.estorebackend.repository.RoleRepository;
 import com.nagarjun.estorebackend.repository.UserRepository;
@@ -28,17 +30,21 @@ public class EstoreBackendApplication implements CommandLineRunner{
 	@Autowired
 	private RoleRepository roleRepository;
 
+	@Autowired
+	private CartRepository cartRepository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(EstoreBackendApplication.class, args);
 	}
 
-	@Bean
-	public BCryptPasswordEncoder bCryptPasswordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
+    @Bean
+    BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
 	@Override
 	public void run(String... args) throws Exception {
+
 		Product[] products = new Product[] {
 			new Product("Table", "It is a table", 1000),
 			new Product("Chair", "It is a chair", 500),
@@ -51,18 +57,27 @@ public class EstoreBackendApplication implements CommandLineRunner{
 		}
 		roleRepository.save(new Role("ADMIN"));
 		roleRepository.save(new Role("USER"));
-		Role role = roleRepository.findById(SecurityConstants.ROLE_ADMIN_ID).get();
+		Role adminRole = roleRepository.findById(SecurityConstants.ROLE_ADMIN_ID).get();
+		Role userRole = roleRepository.findById(SecurityConstants.ROLE_USER_ID).get();
+
 
 		User[] users = new User[] {			
-			new User("admin", "admin", "admin", bCryptPasswordEncoder().encode("admin"), "admin@admin.com", GlobalMethods.dateTimeFormatter(LocalDateTime.now()))
+			new User("admin", "admin", "admin", bCryptPasswordEncoder().encode("admin"), "admin@admin.com", GlobalMethods.dateTimeFormatter(LocalDateTime.now())),
+			new User("test", "test", "test", bCryptPasswordEncoder().encode("test"), "test@test.com", GlobalMethods.dateTimeFormatter(LocalDateTime.now()))
 		};
 
-		
 		for (User user : users) {
-			role.getUsers().add(user);
+			Cart cart = new Cart();
+			cart.setUser(user);
+			if(user.getUsername() == "admin"){
+				adminRole.getUsers().add(user);
+			}
+			userRole.getUsers().add(user);
 			userRepository.save(user);
-			roleRepository.save(role);
+			cartRepository.save(cart);
 		}
+		roleRepository.save(adminRole);
+		roleRepository.save(userRole);
 
 	}
 }
