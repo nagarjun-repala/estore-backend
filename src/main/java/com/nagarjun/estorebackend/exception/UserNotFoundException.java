c20e4f2b57e32e426ae46b98300f38beb0da9a6a
Nagarjun <nagarjun.repala@outlook.com>
2023-01-24 20:16:47 +0530

    Add UserNotFoundException

--- src/main/java/com/nagarjun/estorebackend/exception/UserNotFoundException.java
+++ src/main/java/com/nagarjun/estorebackend/exception/UserNotFoundException.java
@@ -2,6 +2,10 @@ package com.nagarjun.estorebackend.exception;
 
 public class UserNotFoundException extends RuntimeException{
 
+    public UserNotFoundException() {
+        super("Users does not exist in our records");
+    }
+
     public UserNotFoundException(Long id) {
         super("User id: '"  + id + "' does not exist in our records");
     }
