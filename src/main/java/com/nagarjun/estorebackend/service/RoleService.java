e441b14b70b470a1c76d83fdbf73c492b4623a24
nagarjun-repala <nagarjun.repala@outlook.com>
2023-02-05 12:31:41 +0530

    Update role  service with authorize annotation

--- src/main/java/com/nagarjun/estorebackend/service/RoleService.java
+++ src/main/java/com/nagarjun/estorebackend/service/RoleService.java
@@ -1,17 +1,32 @@
 package com.nagarjun.estorebackend.service;
 
 import java.util.List;
-
+import org.springframework.security.access.prepost.PreAuthorize;
 import com.nagarjun.estorebackend.entity.Role;
 
 public interface RoleService {
 
+    @PreAuthorize("hasRole('ADMIN')")
     Role getRoleById(Long roleId);
+
+    @PreAuthorize("hasRole('ADMIN')")
     Role getRoleByName(String roleName);
+
+    @PreAuthorize("hasRole('ADMIN')")
     void assignRole(Long userId, String roleName);
+
+    @PreAuthorize("hasRole('ADMIN')")
     void unassignRole(Long userId, String roleName);
+
+    @PreAuthorize("hasRole('ADMIN')")
     Role createRole(Role role);
+
+    @PreAuthorize("hasRole('ADMIN')")
     Role updateRole(Long roleId, Role role);
+
+    @PreAuthorize("hasRole('ADMIN')")
     void deleteRole(Long roleId);
+
+    @PreAuthorize("hasRole('ADMIN')")
     List<Role> getRoles();
 }
