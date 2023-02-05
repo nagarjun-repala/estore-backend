ed6f17b44c7ad00a1d458bf6777dee2a8436321d
nagarjun-repala <nagarjun.repala@outlook.com>
2023-02-05 12:31:34 +0530

    Update product service with authorize annotation

--- src/main/java/com/nagarjun/estorebackend/service/ProductService.java
+++ src/main/java/com/nagarjun/estorebackend/service/ProductService.java
@@ -1,14 +1,24 @@
 package com.nagarjun.estorebackend.service;
 
 import java.util.List;
+import org.springframework.security.access.prepost.PreAuthorize;
 import com.nagarjun.estorebackend.entity.Product;
 
 public interface ProductService {
 
+    @PreAuthorize("hasRole('ADMIN')")
     Product getProduct(Long productId);
+
+    @PreAuthorize("hasRole('ADMIN')")
     Product createProduct(Product product);
+
+    @PreAuthorize("hasRole('ADMIN')")
     void deleteProduct(Long productId);
+
+    @PreAuthorize("hasRole('ADMIN')")
     Product updateProduct(Long productId, Product product);
+
+    @PreAuthorize("hasRole('ADMIN')")
     List<Product> getProducts();
     
 }
