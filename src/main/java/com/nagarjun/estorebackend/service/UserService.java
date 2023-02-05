6998f0ddbebd009eab7c973c37278f9ea15f89c6
nagarjun-repala <nagarjun.repala@outlook.com>
2023-02-05 12:31:49 +0530

    Update user service with authorize annotation

--- src/main/java/com/nagarjun/estorebackend/service/UserService.java
+++ src/main/java/com/nagarjun/estorebackend/service/UserService.java
@@ -1,9 +1,8 @@
 package com.nagarjun.estorebackend.service;
 
 import java.util.List;
-
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
-
 import com.nagarjun.estorebackend.dto.UserDto;
 import com.nagarjun.estorebackend.entity.User;
 
@@ -14,7 +13,11 @@ public interface UserService {
     User getUser(String username);
     User createUser(User user);
     User updateUser(Long userId, User user);
+
+    @PreAuthorize("hasRole('ADMIN')")
     void deleteUser(Long id);
+
+    @PreAuthorize("hasRole('ADMIN')")
     void delteUser(String username);
     List<User> getAllUsers();
     UserDto getUserDto(String username);
